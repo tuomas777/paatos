813d491d4635f5b1fa0509b9fcae334aecfb6ab1
Tuomas Haapala <tuomas.haapala@anders.fi>
2016-09-22 15:52:20 +0300

    Fix admin UI

--- decisions/admin.py
+++ decisions/admin.py
@@ -2,4 +2,4 @@ from django.apps import apps
 from django.contrib import admin
 
 for model in apps.get_app_config("decisions").get_models():
-    admin.register(model)
+    admin.site.register(model)
