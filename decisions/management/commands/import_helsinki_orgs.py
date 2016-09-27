a6a21f23810c1ee29fe64d23660e571c26e589b3
Tuomas Haapala <tuomas.haapala@anders.fi>
2016-09-27 17:18:11 +0300

    Add initial Helsinki organization importer

---
+++ decisions/management/commands/import_helsinki_orgs.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.db import transaction
+from decisions.importer.helsinki import HelsinkiImporter
+
+
+class Command(BaseCommand):
+    help = 'Imports Helsinki organizations'
+
+    def add_arguments(self, parser):
+        parser.add_argument('filename', type=str)
+
+    def handle(self, *args, **options):
+        #with transaction.atomic():
+        importer = HelsinkiImporter(options)
+        importer.import_organizations(options['filename'])
+        self.stdout.write(self.style.SUCCESS('All done!'))
