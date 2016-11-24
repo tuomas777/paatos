c848732bda0d5151132024da2957826516641020
Tuomas Haapala <tuomas.haapala@anders.fi>
2016-11-24 11:41:50 +0200

    Basic API tests for Posts

--- decisions/factories/organization.py
+++ decisions/factories/organization.py
@@ -1,6 +1,6 @@
 import factory
 from faker import Faker
-from decisions.models import Organization
+from decisions.models import Organization, Post
 
 fake = Faker()
 fake.seed(7)
@@ -14,4 +14,13 @@ class OrganizationFactory(factory.django.DjangoModelFactory):
     abstract = fake.paragraph(nb_sentences=1)
     description = fake.paragraph(nb_sentences=5)
     classification = fake.company_suffix()
-    founding_date = fake.date()
+    founding_date = fake.date_time_this_century(before_now=True, after_now=False)
+
+
+class PostFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Post
+
+    label = fake.word()
+    organization = factory.SubFactory(OrganizationFactory)
+    start_date = fake.date_time_this_century(before_now=True, after_now=False)
